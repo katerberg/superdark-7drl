bea27310d150a0566535f289c5c29723bde61b68
Mark Katerberg <katerberg@fastmail.fm>
2022-03-07 17:35:47 -0600

    Stop time when not moving

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,18 +1,16 @@
 import * as Phaser from 'phaser';
-import {DEPTH, EVENTS, GAME_STATUS, PLAYER} from '../constants';
+import {DEPTH, EVENTS, GAME_STATUS, PLAYER, SCENES} from '../constants';
 import {isDebug} from '../utils/environments';
 import {createFloatingText} from '../utils/visuals';
 import {PlayerLegs} from './PlayerLegs';
 
 export class Player extends Phaser.GameObjects.Sprite {
   legs;
-  cursors;
   hp = PLAYER.MAX_HP;
 
   constructor({scene, x, y, key, angle}) {
     super(scene, x, y, key);
 
-    this.cursors = this.scene.input.keyboard.createCursorKeys();
     this.angle = angle || 0;
     this.depth = DEPTH.PLAYER;
     this.setDisplaySize(PLAYER.HEIGHT * PLAYER.SCALE, PLAYER.WIDTH * PLAYER.SCALE);
@@ -46,7 +44,7 @@ export class Player extends Phaser.GameObjects.Sprite {
   }
 
   handleMovement() {
-    const {up, down, left, right} = this.cursors;
+    const {up, down, left, right} = this.scene.scene.get(SCENES.HUD).playerKeys;
 
     if (up?.isDown || down?.isDown || left?.isDown || right?.isDown) {
       const moveSpeed = isDebug() ? PLAYER.SPEED_DEBUG : PLAYER.SPEED;
