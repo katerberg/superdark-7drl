c1b7fdf2c81ca694f228fc8bf488f91b7e2049aa
Mark Katerberg <katerberg@fastmail.fm>
2022-03-09 10:04:45 -0600

    Working revolver with instant reload

--- src/classes/Weapon.js
+++ src/classes/Weapon.js
@@ -4,28 +4,33 @@ class Weapon {
   damage;
   currentAmmunition;
   maxAmmunition;
+  storedAmmunition;
   useTime;
   reloadTime;
   soundRadiusOfUse;
   active;
 
   constructor(
+    image,
     active = false,
     range = 100,
     size = 20,
     damage = 1,
     currentAmmunition = 999_999_999,
     maxAmmunition = 999_999_999,
+    storedAmmunition = 999_999_999,
     useTime = 1000,
     reloadTime = 1000,
     soundRadiusOfUse = 300,
   ) {
+    this.image = image;
     this.active = active;
     this.range = range;
     this.size = size;
     this.damage = damage;
     this.currentAmmunition = currentAmmunition;
     this.maxAmmunition = maxAmmunition;
+    this.storedAmmunition = storedAmmunition;
     this.useTime = useTime;
     this.reloadTime = reloadTime;
     this.soundRadiusOfUse = soundRadiusOfUse;
@@ -37,6 +42,10 @@ class Weapon {
 
   setInactive() {
     this.active = false;
+  }
+
+  getAmmoText() {
+    return `${this.currentAmmunition}/${this.storedAmmunition > 100_000 ? '∞' : this.storedAmmunition}`;
   }
 
   useAmmo() {
@@ -52,7 +61,7 @@ class Weapon {
 }
 
 export class Revolver extends Weapon {
-  constructor() {
-    super(false, 100, 20, 1, 5, 6);
+  constructor(active) {
+    super('weapon-revolver', active, 100, 20, 1, 5, 6);
   }
 }
